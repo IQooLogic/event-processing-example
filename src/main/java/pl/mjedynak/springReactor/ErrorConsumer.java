c18bb58328e5f8a3e3b7b5a28e7addbdf30fe313
Michal Jedynak <m.jedynak@gmail.com>
2014-09-13 08:43:50 +0200

    cleanup

--- src/main/java/pl/mjedynak/springReactor/ErrorConsumer.java
+++ src/main/java/pl/mjedynak/springReactor/ErrorConsumer.java
@@ -1,8 +1,6 @@
 package pl.mjedynak.springReactor;
 
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.event.Event;
 import reactor.function.Consumer;
 
@@ -14,7 +12,7 @@ public class ErrorConsumer implements Consumer<Event<Exception>> {
     @Override
     public void accept(Event<Exception> exceptionEvent) {
         try {
-            FileUtils.write(new File(Bootstrap.EXCEPTIONS_FILE), exceptionEvent.getHeaders().get("item") + ", " + exceptionEvent.getData() + "\n", true);
+            FileUtils.write(new File(Bootstrap.EXCEPTIONS_FILE), exceptionEvent.getData().getMessage() + "\n", true);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
